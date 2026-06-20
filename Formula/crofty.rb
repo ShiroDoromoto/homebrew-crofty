ee833a6e283d7cdd1d60d407b9217c6a17f28f9f
Shiro Doromoto <ShiroDoromoto@users.noreply.github.com>
2026-06-20 12:08:56 +0900

    wharfy: update crofty 0.15.0

--- Formula/crofty.rb
+++ Formula/crofty.rb
@@ -1,29 +1,29 @@
 class Crofty < Formula
   homepage "https://github.com/ShiroDoromoto/crofty"
-  version "0.14.0"
+  version "0.15.0"
   license "AGPL-3.0"
 
   depends_on "hugo"
 
   on_macos do
     on_arm do
-      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.14.0/crofty_0.14.0_darwin_arm64.tar.gz"
-      sha256 "c9bf8ec78dfc20eae57e8eb30478dab9e9bec6343597389012dd1e88ff0742ec"
+      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.15.0/crofty_0.15.0_darwin_arm64.tar.gz"
+      sha256 "5929ae17686d7ecb27945d5a45822f0bd0bf7b648b85a8b68a423e28f6b850b5"
     end
     on_intel do
-      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.14.0/crofty_0.14.0_darwin_amd64.tar.gz"
-      sha256 "1f521d63c9adf5105425b96ad45b02900300a24c1c0f8ec093271cea34d0a18d"
+      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.15.0/crofty_0.15.0_darwin_amd64.tar.gz"
+      sha256 "c7403c42fa450d63c216e52aef1d453c21bd4f4503b0d6d32dc5d2ab9901cf08"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.14.0/crofty_0.14.0_linux_arm64.tar.gz"
-      sha256 "7e82ec581baa6e0c316ad11b7cfe568089328763d5a41bca14e5cbef5b839580"
+      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.15.0/crofty_0.15.0_linux_arm64.tar.gz"
+      sha256 "4810c3d67b9eb642af46ef2bcafffb3b586df7e163f51ed1b9c39924da8ae742"
     end
     on_intel do
-      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.14.0/crofty_0.14.0_linux_amd64.tar.gz"
-      sha256 "fc65179c2671fb90e01d9fd0c58571cfcd6f993c20709d9a056f010656eb393a"
+      url "https://github.com/ShiroDoromoto/crofty/releases/download/v0.15.0/crofty_0.15.0_linux_amd64.tar.gz"
+      sha256 "c97a4c50e6e584286682c17ff3e23d9bc30143ec3f6a4dd56796c000838f7c29"
     end
   end
 
@@ -32,6 +32,6 @@ class Crofty < Formula
   end
 
   test do
-    assert_match "0.14.0", shell_output("#{bin}/crofty version")
+    assert_match "0.15.0", shell_output("#{bin}/crofty version")
   end
 end
